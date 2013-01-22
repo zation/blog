a9c2dee7e6f1b39ccedd2ebddd024d66b01f6324
Liu Yang <zation1@gmail.com>
2013-01-22 15:56:53 +0800

    add prettify

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,5 +1,10 @@
 //= require "jquery-1.8.3.min"
+//= require "prettify"
 
 $(function() {
   $('.article-content a').attr('target', '_blank');
+
+  $('.article-content code, .article-content pre').addClass('prettyprint');
+  $('.article-content pre').addClass('linenums');
+  prettyPrint();
 });
